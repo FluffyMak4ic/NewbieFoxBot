57edac98e3b760822022295ca7d5187089a77d8e
Maxim Gysev <mak4ic@yandex.ru>
2020-10-28 22:48:19 +0500

    yip

--- commands/fun/yip.js
+++ commands/fun/yip.js
@@ -1,9 +1,12 @@
+const { MessageAttachment } = require('discord.js');
+
 module.exports = {
     name: "yip",
     category: "fun",
     description: "Yip yip yip",
     run: async (client, message, args) => {
-        const attachment = new MessageAttachment('https://tenor.com/view/bark-furry-yip-gif-12332058');
+        await message.delete();
+        const attachment = new MessageAttachment('https://tenor.com/view/bark-furry-yip-gif-12332058' + ".gif");
         // Send the attachment in the message channel
         message.channel.send(attachment);
     }
